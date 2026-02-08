647d917e6373c6457b6c8ee8e6d8350944b8f935
paprzyby <pawel.przybyla2005@gmail.com>
2026-02-08 15:10:21 +0100

    added rest of operations for bigint

--- level0/bigint/bigint.cpp
+++ level0/bigint/bigint.cpp
@@ -34,6 +34,70 @@ bigint bigint::operator+=(const bigint &other)
     return (*this);
 }
 
+bigint bigint::operator++()
+{
+    this->str = std::to_string(std::stoul(this->str) + 1);
+    return (*this);
+}
+
+bigint bigint::operator++(int)
+{
+    bigint result = bigint(*this);
+
+    this->str = std::to_string(std::stoul(this->str) + 1);
+    return (result);
+}
+
+bigint bigint::operator<<(const unsigned int i) const
+{
+    bigint result = bigint();
+
+    result.str = std::to_string(std::stoul(this->str) << i);
+    return (result);
+}
+
+bigint bigint::operator<<=(const unsigned int i)
+{
+    this->str = std::to_string(std::stoul(this->str) << i);
+    return (*this);
+}
+
+bigint bigint::operator>>=(const bigint &other)
+{
+    this->str = std::to_string(std::stoul(this->str) >> std::stoul(other.str));
+    return (*this);
+}
+
+bool bigint::operator<(const bigint &other) const
+{
+    return (std::stoul(this->str) < std::stoul(other.str));
+}
+
+bool bigint::operator<=(const bigint &other) const
+{
+    return (std::stoul(this->str) <= std::stoul(other.str));
+}
+
+bool bigint::operator>(const bigint &other) const
+{
+    return (std::stoul(this->str) > std::stoul(other.str));
+}
+
+bool bigint::operator>=(const bigint &other) const
+{
+    return (std::stoul(this->str) >= std::stoul(other.str));
+}
+
+bool bigint::operator==(const bigint &other) const
+{
+    return (std::stoul(this->str) == std::stoul(other.str));
+}
+
+bool bigint::operator!=(const bigint &other) const
+{
+    return (std::stoul(this->str) != std::stoul(other.str));
+}
+
 std::ostream &operator<<(std::ostream &output, const bigint &obj)
 {
     output << obj.str;
--- level0/bigint/bigint.hpp
+++ level0/bigint/bigint.hpp
@@ -11,8 +11,20 @@ class bigint
         bigint(const bigint &other);
         ~bigint();
 
-        bigint operator +(const bigint &other) const;
-        bigint operator +=(const bigint &other);
+        bigint operator+(const bigint &other) const;
+        bigint &operator+=(const bigint &other);
+        bigint &operator++();
+        bigint operator++(int);
+        bigint operator<<(const unsigned int i) const;
+        bigint &operator<<=(const unsigned int i);
+        bigint &operator>>=(const bigint &other);
+
+        bool operator<(const bigint &other) const;
+        bool operator<=(const bigint &other) const;
+        bool operator>(const bigint &other) const;
+        bool operator>=(const bigint &other) const;
+        bool operator==(const bigint &other) const;
+        bool operator!=(const bigint &other) const;
     
     public:
         std::string str;
