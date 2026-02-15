49056690496c78f8ac71eed2315165040114ed52
paprzyby <pawel.przybyla2005@gmail.com>
2026-02-15 14:02:01 +0100

    afsaf

---
+++ level0/vect2/vect2.cpp
@@ -0,0 +1,57 @@
+#include "vect2.hpp"
+
+vect2::vect2() : _n1(0), _n2(0)
+{
+    
+}
+
+vect2::vect2(int n1, int n2)
+{
+    this->_n1 = n1;
+    this->_n2 = n2;
+}
+
+vect2::vect2(vect2 &other)
+{
+    this->_n1 = other._n1;
+    this->_n2 = other._n2;
+}
+
+vect2::~vect2()
+{
+    
+}
+
+vect2 vect2::operator=(vect2 &other)
+{
+    if (this != &other)
+    {
+        this->_n1 = other._n1;
+        this->_n2 = other._n2;
+    }
+    return (*this);
+}
+
+int vect2::operator[](int i) const
+{
+    if (i == 0)
+        return (this->_n1);
+    else if (i == 1)
+        return (this->_n2);
+    return (0);
+}
+
+int vect2::operator[](int i)
+{
+    if (i == 0)
+        return (this->_n1);
+    else if (i == 1)
+        return (this->_n2);
+    return (0);
+}
+
+std::ostream &operator<<(std::ostream &os, const vect2 &v)
+{
+    std::cout << "{" << v[0] << ", " << v[1] << "}";
+    return (os);
+}
---
+++ level0/vect2/vect2.hpp
@@ -0,0 +1,25 @@
+#ifndef VECT2_HPP
+# define VECT2_HPP
+
+# include <iostream>
+
+class   vect2
+{
+    public:
+        vect2();
+        vect2(int n1, int n2);
+        vect2(vect2 &other);
+        ~vect2();
+
+        vect2 operator=(vect2 &other);
+        int operator[](int i) const;
+        int operator[](int i);
+
+    private:
+        int _n1;
+        int _n2;
+};
+
+std::ostream &operator<<(std::ostream &os, const vect2 &v);
+
+#endif
